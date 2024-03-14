914f3816730bd6c8ccc011b8df0e4f4f11198a6d
Arjun Yadav <yadavarjun182@gmail.com>
2024-03-14 22:12:16 +0530

    done

---
+++ rtk_projectone/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Task from './components/Task'
+import TaskList from './components/TaskList'
+
+function App() {
+
+
+  return (
+    <>
+    <Task/>
+    <TaskList/>
+    </>
+  )
+}
+
+export default App
---
+++ rtk_projectone/src/components/TaskList.jsx
@@ -0,0 +1,37 @@
+import { useSelector, useDispatch } from "react-redux";
+import { deleteTodo } from "../features/todo/todoSlice";
+
+import React from "react";
+
+const TaskList = () => {
+  const tasks = useSelector((state) => state.todo.tasks);
+  console.log(tasks);
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(deleteTodo(id));
+  };
+
+  return (
+    <div className="tasklist">
+      <div className="display-tasks">
+        <h3>Your tasks:</h3>
+        <ul className="tasks">
+          {tasks?.map((task) => (
+            <li className="task" key={task.id}>
+              {task.text}
+              <button
+                className="delete-btn"
+                onClick={() => handleDelete(task.id)}
+              >
+                delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
